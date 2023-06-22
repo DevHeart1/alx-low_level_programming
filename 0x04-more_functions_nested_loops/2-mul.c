fb7ba02cdfc8e1c439a34ed97d1618934d20ff3e
DevHeart1 <obasipeace412@yahoo.com>
2023-06-22 06:41:00 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,10 @@
 /**
  * mul - multiplies two integers.
  * @a,b - character
- * Return: Always 0.
+ * Return: a*b.
  */
 
 int mul(int a, int b)
 {
-	_putchar("%d", a*b);
-return (0);
+	return (a * b);
 }
