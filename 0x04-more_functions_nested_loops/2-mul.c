cad579873f3e361667aac7375b452aea6f5d336d
DevHeart1 <obasipeace412@yahoo.com>
2023-06-22 06:24:42 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a,b - character
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int i = a*b
+
+	if ((a >= '0' || a <= '0') && (b >= '0' || b <=0))
+		_putchar("%d", i);
+return (0);
+}
