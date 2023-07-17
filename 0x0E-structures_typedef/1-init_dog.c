9dd4061fe4e4e07bebca15d5675021d50caa6348
DevHeart1 <obasipeace412@yahoo.com>
2023-07-17 16:02:34 -0700

    added the second file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "main.h"
+#include "dog.h"
 
 /**
  * init_dog - this function initialize a variable of type struct dog
- * @d: pointer to the variable 
+ * @d: pointer to the variable
  * @name: value of the stuct dog's name field.
- * @age: value of the struct dog's age field. 
+ * @age: value of the struct dog's age field.
  * @owner: value of the struc dog's owner field.
  */
 
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,12 @@
 #ifndef DOG_H
-#define DOG_H 
+#define DOG_H
 
 /**
  * struct dog - A dog data structure
  * @name: The name of the dog
  * @age: The age of the dog
  * @owner: The name of the dog's owner
- */
-
+ */
 struct dog
 {
 	char *name;
