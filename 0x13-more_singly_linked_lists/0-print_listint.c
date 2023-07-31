681f4159140c23a9563baa1e0192e0560d26bfc5
DevHeart1 <obasipeace412@yahoo.com>
2023-07-31 13:56:08 -0700

    added readme and header file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * size_t print_listin - prints all the elements of a listint_t list.
+ * @h: head
+ *
+ * Return: the number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
