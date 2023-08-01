9f8e1d9c2a1bcfc6e8fd144e7de68bd87fa8b77b
DevHeart1 <obasipeace412@yahoo.com>
2023-08-01 13:59:20 -0700

    added tast 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,18 +7,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tempoorary;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		temporary = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
-		*head = temporary;
+		*head = temp;
 	}
 
 	*head = NULL;
-
 }
