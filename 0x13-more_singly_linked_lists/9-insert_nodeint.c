ad81a0b494844b1a5cc3963b79db4e3e51802f0d
DevHeart1 <obasipeace412@yahoo.com>
2023-08-01 16:19:06 -0700

    eddited task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,37 +9,37 @@
  *
  * Return: The address of the new node, or NULL if it failed
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int g;
-	listint_t *ptrr, *new_node;
+	unsigned int i;
+	listint_t *ptr, *new;
 
 	if (head == NULL)
 		return (NULL);
 
 	if (idx != 0)
 	{
-		ptrr = *head;
-		for (g = 0; g < idx - 1 && ptrr != NULL; g++)
-			ptrr = ptrr->next;
-		if (ptrr == NULL)
+		ptr = *head;
+		for (i = 0; i < idx - 1 && ptr != NULL; i++)
+			ptr = ptr->next;
+		if (ptr == NULL)
 			return (NULL);
 	}
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = n;
+	new->n = n;
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
-	new_node->next = ptrr->next;
-	ptrr->next = new_node;
-	return (new_node);
-}
+	new->next = ptr->next;
+	ptr->next = new;
+	return (new);
 }
