f2974a1d5cabfe56472fc157dd2850924defba2d
DevHeart1 <obasipeace412@yahoo.com>
2023-08-11 13:43:23 -0700

    added file 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,49 @@
 #include "main.h"
 
 /**
+ * read_textfile - This function reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: This represents the  name of the file to read
+ * @letters: This represents the number of letters
  *
- *
- *
- *
- *
- *
- *
+ * Return: the number of letters it could read or 0 if failed
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int file_D;
-	char *buffer_space;
-	ssize_t bytes_R;
-	ssize_t bytes_W;
+ssize_t read_textfile(const char *filename, size_t letters) {
+    int file_D;
+    char *buffer_space;
+    ssize_t bytes_R;
+    ssize_t bytes_W;
 
+    if (filename == NULL)
+        return (0);
 
+    file_D = open(filename, O_RDONLY);
+    if (file_D == -1)
+        return (0); // Failed to open the file
 
-	file_D = open("filename", O_RDONLY);
-	if (file_D == NULL)
-		return (0); /* The program returns 0 if file can not be opened. */
+    buffer_space = malloc(sizeof(char) * (letters + 1));
+    if (buffer_space == NULL) {
+        close(file_D);
+        return (0); // Failed to allocate memory
+    }
 
-	if (filename == NULL)
-		return (0);
+    bytes_R = read(file_D, buffer_space, letters);
+    if (bytes_R == -1) {
+        close(file_D);
+        free(buffer_space);
+        return (0); // Failed to read from the file
+    }
 
-	buffer_space = malloc(sizeof(char) * (letters + 1));
-	if (buffer_space == NULL)
-		return (0);
+    bytes_W = write(STDOUT_FILENO, buffer_space, bytes_R);
+    if (bytes_W == -1) {
+        close(file_D);
+        free(buffer_space);
+        return (0); // Failed to write to standard output
+    }
 
-	bytes_R = read(file_D, buffer_space, letters);
-	if (bytes_R == NULL)
-	{
-		close(file_D);
-		return (0);
-	}
+    close(file_D);
+    free(buffer_space);
 
-	bytes_W = write(STDOUT_FILENO, buffer_space, letters - 1);
-	if (bytes_W == NULL)
-		return (0);
-
-	close(file_D);
-	free(buffer_space);
-
-	return (bytes_r);
+    return(bytes_W);
 }
